be8bae466af57f46bb778393a5de3dd3375cb337
Rohit Rao <rohit.rao@joshtechnologygroup.com>
2025-08-06 09:16:22 +0530

    RR_A2_04:Fix the password hashing

--- todoapp/users/models.py
+++ todoapp/users/models.py
@@ -12,8 +12,7 @@ class UserManager(BaseUserManager):
         Create and save a user with the given email, and password.
         """
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
+        user = self.model(email=email,password = password, **extra_fields)
         user.save(using=self._db)
         return user
 
